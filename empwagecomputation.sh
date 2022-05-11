80f73412fd2cee9f63f17ff1bb1a74b5342e6723
pachuplk <vanguardplk@gmail.com>
2022-05-11 23:44:21 +0530

    employee wage computation

---
+++ empwagecomputation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ echo " Welcome to Employee Wage Computation Program "
+
+
+# Check Employee is Present or Absent
+ispresent=1
+
+randomnumber=$(($RANDOM%2))
+
+echo $randomnumber
+
+if [ $randomnumber -eq $ispresent ]
+
+then
+
+	echo "employee is present"
+
+else
+
+ 	echo "employee is absent"
+
+fi
